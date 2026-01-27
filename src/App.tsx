dbd45c1fe78f3517fef473fea52f9588200fdf2d
Chau <hoaichau20@hotmail.com>
2026-01-27 12:26:18 +0100

    photo+texte

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,25 @@ import "./App.css";
 function App() {
   return (
     <>
-  
-      <img src="/images/logo.png" alt="Logo" />
+      <div>
+        <img src="/images/logo.png" alt="Logo" className="w-50" />
+      </div>
+
+      <div className="flex items-center gap-8">
+
+      <div className="flex-1">
+        <h2>
+          Sorcier britannique, survivant de la malédiction de Voldemort, élève
+          de Gryffondor devenu le symbole de la résistance contre les forces du
+          mal.
+        </h2>
+      </div>
+
+      <div className="flex-1">
+        <img src="/images/presentation.jpeg" alt="Presentation" />
+      </div>
+
+      </div>
     </>
   );
 }
