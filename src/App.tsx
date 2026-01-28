c4f3e620cd520822140d8a517f2249d851730585
Patrice Philetas <patricephiletas@MacBook-Pro-de-Patrice-2.local>
2026-01-28 16:21:21 +0100

    nouvelle structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./index.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Chronologie from "./pages/Chronologie";
@@ -11,12 +11,15 @@ function App() {
   return (
     <>
       <header>
-        <div className="bg-gray-200 flex items-center justify-center p-4 mb-10">
+        <Link to="/"
+        className="bg-gray-200 flex items-center justify-center p-4 mb-10">
+
           <img
             src="/images/logo.png"
             alt="Logo"
             className="w-60 items-center hover:scale-110"
           />
+          </Link>
 
           <Link
             to="/chronologie"
@@ -38,28 +41,10 @@ function App() {
           >
             Formules Magiques
           </Link>
-        </div>
+        
       </header>
 
       <main>
-        <div className="flex items-center gap-8 bg-red-900 p-10 rounded-4xl mx-20">
-          <div className="flex-1 ml-15">
-            <p className="text-white leading-loose mr-8 first-letter:font-stretch-50% first-letter:text-8xl first-letter:float-left first-letter:mr-3 first-letter:-mt-1 font-HarryP first-letter:text-yellow-500">
-              Sorcier britannique, survivant de la malédiction de Voldemort,
-              élève de Gryffondor devenu le symbole de la résistance contre les
-              forces du mal.
-            </p>
-          </div>
-
-          <div className="flex-1">
-            <img
-              src="/images/presentation.jpeg"
-              alt="Presentation"
-              className="rounded-4xl hover:scale-115 hover:cursor-pointer hover:shadow-lg"
-            />
-          </div>
-        </div>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chronologie" element={<Chronologie />} />
@@ -72,4 +57,3 @@ function App() {
 }
 
 export default App;
-
