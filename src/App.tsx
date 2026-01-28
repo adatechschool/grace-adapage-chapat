d969eeeed500e71e0c95c6b6ac9fe19f91a7634a
Patrice Philetas <patricephiletas@MacBook-Pro-de-Patrice-2.local>
2026-01-28 08:31:07 +0100

    divers essais tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [btn, setBtn] = useState("");
@@ -7,38 +8,37 @@ function App() {
   return (
     <>
       <header>
-        <div>
-          <img src="/images/logo.png" alt="Logo" className="w-50" />
-        </div>
+        <div className="bg-gray-200 flex items-center justify-center p-4 mb-10">
 
-        <div className="flex gap-20">
-          <button className="px-4 py-2 bg-blue-400 ml-20" onClick={() => {}}>
+          <img src="/images/logo.png" alt="Logo" className="w-60 items-center hover:scale-110" />
+
+          <button className="px-4 py-2 rounded-2xl bg-yellow-400 hover:bg-yellow-500 ml-20" onClick={() => {}}>
             Chronologie
           </button>
-          <button className="px-4 py-2 bg-blue-400" onClick={() => {}}>
+          <button className="px-4 py-2 rounded-2xl bg-yellow-400 hover:bg-yellow-500 ml-20" onClick={() => {}}>
             Amis Proches
           </button>
-          <button className="px-4 py-2 bg-blue-400" onClick={() => {}}>
+          <button className="px-4 py-2 rounded-2xl bg-yellow-400 hover:bg-yellow-500 ml-20" onClick={() => {}}>
             Records
           </button>
-          <button className="px-4 py-2 bg-blue-400" onClick={() => {}}>
+          <button className="px-4 py-2 rounded-2xl bg-yellow-400 hover:bg-yellow-500 ml-20" onClick={() => {}}>
             Formules Magiques
           </button>
         </div>
       </header>
 
       <body>
-        <div className="flex items-center gap-8">
-          <div className="flex-1 ml-20">
-            <h2>
-              Sorcier britannique, survivant de la malédiction de Voldemort,
-              élève de Gryffondor devenu le symbole de la résistance contre les
-              forces du mal.
-            </h2>
+        <div className="flex items-center gap-8 bg-red-900 p-10 rounded-4xl mx-20">
+          <div className="flex-1 ml-15">
+            <p className="text-white leading-loose mr-8 first-letter:font-stretch-50% first-letter:text-8xl first-letter:float-left first-letter:mr-3 first-letter:-mt-1 font-HarryP first-letter:text-yellow-500">
+    Sorcier britannique, survivant de la malédiction de Voldemort, élève de Gryffondor devenu le symbole de la résistance contre les forces du mal.
+            </p>
           </div>
 
+          
+
           <div className="flex-1">
-            <img src="/images/presentation.jpeg" alt="Presentation" />
+            <img src="/images/presentation.jpeg" alt="Presentation" className="rounded-4xl hover:scale-115 hover:cursor-pointer hover:shadow-lg" />
           </div>
         </div>
       </body>
