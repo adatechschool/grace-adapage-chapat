9fbd3b250818173d7744139388d408c0fe88e404
Patrice Philetas <patricephiletas@MacBook-Pro-de-Patrice-2.local>
2026-01-29 10:21:28 +0100

    3e commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ function Home() {
 
           <div className="flex-1">
             <img
-              src="/images/presentation.jpeg"
+              src="/src/assets/images/presentation.jpeg"
               alt="Presentation"
               className="rounded-4xl hover:scale-105 hover:cursor-pointer hover:shadow-lg"
             />
@@ -22,29 +22,5 @@ function Home() {
 }
 
 export default Home;
-
-
-
-// export default Home;
-
-// import Card from "../components/Card";
-// import { home } from "../data/home";
-// import type { HomeItem } from "../type/interfaces";
-
-// const HomePage = () => {
-//   return (
-//     <div className="flex items-center gap-8 bg-red-900 p-10 rounded-4xl mx-20">
-//     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
-//       {home.map((item: HomeItem) => (
-//         <Card
-//           key={item.id}
-//           description={item.description}
-//           image className="rounded-4xl hover:scale-105 hover:cursor-pointer hover:shadow-lg"={item.photo}
-//         />
-//       ))}
-//     </div>
-//     </div>
-//   );
-// };
 
 
