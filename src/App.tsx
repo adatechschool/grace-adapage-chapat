34c615208a23026694aaf4ab68728687000532d5
Patrice Philetas <patricephiletas@MacBook-Pro-de-Patrice-2.local>
2026-01-29 15:04:07 +0100

    import records

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
         </div>  
       </header>
 
-      <main>
+      <main className="max-w-8xl mx-auto px-6">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chronologie" element={<Chronologie />} />
