6480e794d448619af67c3c0bb4e12ced8bfd1063
Patrice Philetas <patricephiletas@MacBook-Pro-de-Patrice-2.local>
2026-01-30 10:27:50 +0100

    actualisation vendredi matin

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import type { CardProps } from "../type/interfaces";
 
 const Card = ({ title, subtitle, description, image }: CardProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition p-4 flex flex-col">
       {image && (
@@ -15,10 +18,26 @@ const Card = ({ title, subtitle, description, image }: CardProps) => {
       {subtitle && <p className="text-sm text-gray-500 mb-2">{subtitle}</p>}
 
       <p
-        className="text-base first-letter:font-stretch-50% first-letter:text-6xl first-letter:float-left first-letter:mr-3 first-letter:-mt-2 font-HarryP first-letter:text-yellow-500"
+        className={`
+          text-base
+          font-HarryP
+          first-letter:text-3xl
+          first-letter:mr-1
+          first-letter:-mt-1
+          first-letter:font-stretch-50%
+          first-letter:text-yellow-500
+          ${isExpanded ? "line-clamp-none" : "line-clamp-10"}
+        `}
       >
         {description}
       </p>
+
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className="mt-2 self-start text-sm text-red-800 hover:underline font-semibold"
+      >
+        {isExpanded ? "Réduire" : "Lire plus"}
+      </button>
     </div>
   );
 };
--- src/data/Records.ts
+++ src/data/Records.ts
@@ -1,33 +1,36 @@
 import type { RecordItem } from "../type/interfaces";
 
 export const records: RecordItem[] = [
-{
+  {
     id: "1",
-    titre:"Survivant",
+    titre: "Survivant",
     date: "31/10/1981",
-    description: "Plus jeune survivant d’un Avada Kedavra à 1 an.",
+    description:
+      "À l’âge d’un an, Harry Potter devient le seul sorcier connu à avoir survécu directement au sortilège de mort Avada Kedavra. Lancé par Lord Voldemort lors de l’attaque de Godric’s Hollow, le sort est neutralisé par le sacrifice de Lily Potter, provoquant un rebond magique inédit. Cet événement marque la chute temporaire de Voldemort et constitue un cas unique dans l’histoire de la magie.",
     photo: "/src/assets/images/records/Kedavra.jpeg"
   },
   {
     id: "2",
-    titre:"Quidditch",
+    titre: "Quidditch",
     date: "1991-1992",
-    description: "Premier à attraper le Vif d’Or dès son premier match à 11 ans.",
+    description:
+      "À seulement 11 ans, Harry Potter devient le plus jeune Attrapeur de Gryffondor depuis plus d’un siècle. Lors de son tout premier match officiel de Quidditch, il attrape le Vif d’Or, mettant fin à la rencontre et assurant la victoire de son équipe. Ce fait d’armes précoce établit un record de précocité sportive à Poudlard.",
     photo: "/src/assets/images/records/quidditch_first.jpg"
   },
   {
     id: "3",
-    titre:"Tournoi des Trois Sorciers",
+    titre: "Tournoi des Trois Sorciers",
     date: "1994-1995",
-    description: "Plus jeune champion à 14 ans.",
+    description:
+      "Sélectionné malgré lui par la Coupe de Feu, Harry Potter devient à 14 ans le plus jeune champion de l’histoire du Tournoi des Trois Sorciers. Confronté à des épreuves normalement réservées à des sorciers majeurs, il survit à un dragon, à une épreuve sous-marine et à un labyrinthe enchanté, remportant le tournoi au terme d’une compétition exceptionnellement dangereuse.",
     photo: "/src/assets/images/records/trois_sorciers.jpg"
   },
   {
     id: "4",
-    titre:"Patronus",
+    titre: "Expecto Patronum",
     date: "1995-1996",
-    description: "Plus jeune à produire un Patronus corporel complet à 15 ans.",
+    description:
+      "À l’âge de 15 ans, Harry Potter réussit à produire un Patronus corporel complet, une forme de magie défensive extrêmement avancée. Capable de repousser plusieurs Détraqueurs simultanément, cet exploit est rarissime pour un élève de Poudlard et généralement réservé à des sorciers adultes expérimentés, ce qui établit un record de maîtrise précoce.",
     photo: "/src/assets/images/records/patronus.jpg"
   }
-
 ];
